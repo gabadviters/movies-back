40470f8fd8e366ad61cf0dccb0a31d767fd5e360
Gabriel Quiroga <capacitaciones06.10@adviters.com>
2024-07-31 17:02:17 -0300

    funcional

--- src/reviews/dto/create-review.dto.ts
+++ src/reviews/dto/create-review.dto.ts
@@ -1,11 +1,15 @@
+import { ApiProperty } from "@nestjs/swagger"
 import { IsString } from "class-validator"
 
 export class CreateReviewDto {
+    @ApiProperty()
     @IsString()
     review:string
 
+    @ApiProperty()
     @IsString()
     calification:string
+
 
     @IsString()
     create_at:string
